03d314f6b23fb91d8f839656d986567936776e53
Fishale <fishale2500@gmail.com>
2023-04-17 19:01:02 +0700

    rebuild

---
+++ src/test/java/AppCardDeliveryTest.java
@@ -0,0 +1,36 @@
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import static com.codeborne.selenide.Selenide.*;
+
+
+public class AppCardDeliveryTest {
+
+    public String generateDate(int days) {
+        return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    }
+
+    @Test
+    void shouldRegisterCardDelivery(){
+        Configuration.holdBrowserOpen = true;
+        String planningDate = generateDate(6);
+        open("http://localhost:9999");
+        $("[placeholder=\"Город\"]").setValue("Кемерово");
+        $("[placeholder=\"Дата встречи\"]").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("[placeholder=\"Дата встречи\"]").setValue(planningDate);
+        $("[name='name']").setValue("Иванов Иван");
+        $("[name='phone']").setValue("+79236026062");
+        $x("//*[contains(text(), 'Я соглашаюсь')]").click();
+        $x("//*[contains(text(), 'Забронировать')]").click();
+        $("[class=\"notification__content\"]")
+                .shouldHave(Condition.text("Встреча успешно забронирована на " + planningDate), Duration.ofSeconds(15))
+                .should(Condition.appear);
+    }
+
+}
